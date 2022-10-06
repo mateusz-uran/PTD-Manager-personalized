8fc65672eeb8775ab724da0a8fdc297b27f5f1c2
mateusz-uran <mateusz.uranowski@onet.pl>
2022-10-06 09:06:54 +0200

    refactor: init project

---
+++ src/main/java/io/github/mateuszuran/ptdmanagerpersonalized/PtdManagerPersonalizedApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateuszuran.ptdmanagerpersonalized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtdManagerPersonalizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtdManagerPersonalizedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mateuszuran/ptdmanagerpersonalized/PtdManagerPersonalizedApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateuszuran.ptdmanagerpersonalized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtdManagerPersonalizedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
